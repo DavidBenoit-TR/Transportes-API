103cb7cfebf2c0d4d046b4b8c5dfb4ad4b6a8d49
David Benoit <mdavid.benoit@gmail.com>
2024-11-21 13:58:10 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/CamionesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transportes_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CamionesController : ControllerBase
+    {
+    }
+}
